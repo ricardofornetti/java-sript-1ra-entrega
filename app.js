c3cf0f2f26bf88aef7dece7ca912eb3f4e3dad48
RicardoFornetti <ricardofornetti@hotmail.com.ar>
2022-03-16 23:08:08 -0300

    mejorando app.js

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ for(let i=0; i<11; i++){
 }
 */
 
+/*
 var menu="MENU\n";
 menu+=" 1) SUMA\n";
 menu+=" 2) RESTA\n";
@@ -60,4 +61,87 @@ do {
 
     }
 } while (cod!=4);
+*/
+
+
+
+let num1=Number(prompt("Ingrese primer numero"));
+let num2=Number(prompt("Ingrese segundo numero"));
+let opcion ="";
+
+do {
+
+    alert(" 1) SUMA\n"+" 2) RESTA\n"+" 3) MULTIPLICACIÓN\n"+" 4) DIVISIÓN\n"+" 5) SALIR\n"+"ELIJA OPCIÓN");
+    
+    menu=parseInt(prompt("ELIJA OPCIÓN"));
+
+    switch (menu) {
+        case 1:            
+            let suma = num1 + num2;            
+            alert("La suma de " + num1 + " + " + num2 + " es = " + suma);
+            opcion = prompt("Desea ingresar otros numeros (S/N)")
+            if ((opcion == "S") || (opcion == "s")) {
+                num1=Number(prompt("Ingrese primer numero"));
+                num2=Number(prompt("Ingrese segundo numero"));                
+            } else {
+                break;                
+            }
+            break;
+            
+        case 2:
+            let resta = num1 - num2;            
+            alert("La resta de " + num1 + " - " + num2 + " es = " + resta)
+            opcion = prompt("Desea ingresar otros numeros (S/N)")
+            if ((opcion == "S") || (opcion == "s")) {
+                num1=Number(prompt("Ingrese primer numero"));
+                num2=Number(prompt("Ingrese segundo numero"));                
+            } else {
+                break;                
+            }
+            break;
+
+        case 3:
+            if ((num1==0) || (num2==0)) {
+                alert("Multiplicacion por 0. INCORRECTO")
+                
+            } else {
+                let mult = num1 * num2;
+                alert("El producto de " + num1 + " * " + num2 + " es = " + mult)
+            }       
+            opcion = prompt("Desea ingresar otros numeros (S/N)")
+            if ((opcion == "S") || (opcion == "s")) {
+                num1=Number(prompt("Ingrese primer numero"));
+                num2=Number(prompt("Ingrese segundo numero"));                
+            } else {
+                break;                
+            }
+            break;
+
+        case 4:
+            if (num2==0) {
+                alert("Division por 0. INCORRECTO")
+                
+            } else {
+                let div = num1 / num2;
+                alert("La división de " + num1 + " / " + num2 + " es = " + div)
+            }       
+            opcion = prompt("Desea ingresar otros numeros (S/N)")
+            if ((opcion == "S") || (opcion == "s")) {
+                num1=Number(prompt("Ingrese primer numero"));
+                num2=Number(prompt("Ingrese segundo numero"));                
+            } else {
+                break;                
+            }
+            break;
+
+        case 5:
+            alert("HASTA LA PRÓXIMA");
+            break;
+
+        default:
+            alert("OPCION INCORRECTA")    
+
+    }
+} while (menu!=5);
+
 
